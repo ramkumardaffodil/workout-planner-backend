da0c93f9b90310f25c37b71a4fbed8aef3faf53a
ramkumardaffodil <ram.kumar@daffodilsw.com>
2024-09-26 19:04:41 +0530

    Uncommented open ai feature/endpoints

--- src/controllers/workout/createWorkout.ts
+++ src/controllers/workout/createWorkout.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { isEmptyFields } from '../../utils/errorUtils';
 import UserDetails from '../../models/user/UserDetails';
-// import OpenAI from 'openai';
+import OpenAI from 'openai';
 import PlansSchema from '../../models/workout/Workout';
 
 export const createWorkout = async (req: Request, res: Response) => {
@@ -88,38 +88,38 @@ export const createWorkout = async (req: Request, res: Response) => {
 					]
 	    `;
 
-		// const openai = new OpenAI({
-		// 	apiKey: process.env.OPEN_API_KEY,
-		// });
+		const openai = new OpenAI({
+			apiKey: process.env.OPEN_API_KEY,
+		});
 
-		// const aiResponse = await openai.chat.completions.create({
-		// 	messages: [{ role: 'user', content: prompt }],
-		// 	model: 'gpt-4o-mini',
-		// 	max_tokens: 3000,
-		// });
-		// //@ts-ignore
-		// let plans = aiResponse?.choices[0]?.message?.content
-		// 	.replace(/```json/g, '')
-		// 	.replace(/```/g, '')
-		// 	.trim();
-		// plans = JSON.parse(plans);
-		// //@ts-ignore
-		// const workouts = await PlansSchema.create({
-		// 	userId,
-		// 	userDetailId: userDetails._id,
-		// 	//@ts-ignore
-		// 	plans: plans.map((dayPlan: any) => ({
-		// 		day: dayPlan.day,
-		// 		bodyPart: dayPlan.bodyPart,
-		// 		exercises: dayPlan.exercises.map((exercise: any) => ({
-		// 			name: exercise.name,
-		// 			sets: exercise.sets,
-		// 			reps: exercise.reps,
-		// 			bodyPart: exercise.bodyPart,
-		// 			description: exercise.description,
-		// 		})),
-		// 	})),
-		// });
+		const aiResponse = await openai.chat.completions.create({
+			messages: [{ role: 'user', content: prompt }],
+			model: 'gpt-4o-mini',
+			max_tokens: 3000,
+		});
+		//@ts-ignore
+		let plans = aiResponse?.choices[0]?.message?.content
+			.replace(/```json/g, '')
+			.replace(/```/g, '')
+			.trim();
+		plans = JSON.parse(plans);
+		//@ts-ignore
+		const workouts = await PlansSchema.create({
+			userId,
+			userDetailId: userDetails._id,
+			//@ts-ignore
+			plans: plans.map((dayPlan: any) => ({
+				day: dayPlan.day,
+				bodyPart: dayPlan.bodyPart,
+				exercises: dayPlan.exercises.map((exercise: any) => ({
+					name: exercise.name,
+					sets: exercise.sets,
+					reps: exercise.reps,
+					bodyPart: exercise.bodyPart,
+					description: exercise.description,
+				})),
+			})),
+		});
 		return res.status(200).json({ workouts: 'test' });
 	} else {
 		return res.status(403).json({ err: 'Authroization error' });
@@ -142,19 +142,19 @@ export const getAllPlans = async (req: Request, res: Response) => {
 export const getSuggestions = async (req: Request, res: Response) => {
 	if (req.isAuth) {
 		const { prompt } = req.body;
-		// const openai = new OpenAI({
-		// 	apiKey: process.env.OPEN_API_KEY,
-		// });
+		const openai = new OpenAI({
+			apiKey: process.env.OPEN_API_KEY,
+		});
 
-		// const aiResponse = await openai.chat.completions.create({
-		// 	messages: [{ role: 'user', content: prompt }],
-		// 	model: 'gpt-4o-mini',
-		// 	max_tokens: 4000,
-		// });
-		// console.log('response',aiResponse)
-		// if (aiResponse) {
-		// 	return res.status(200).json({ suggestions:aiResponse.choices[0].message.content });
-		// }
+		const aiResponse = await openai.chat.completions.create({
+			messages: [{ role: 'user', content: prompt }],
+			model: 'gpt-4o-mini',
+			max_tokens: 4000,
+		});
+		console.log('response',aiResponse)
+		if (aiResponse) {
+			return res.status(200).json({ suggestions:aiResponse.choices[0].message.content });
+		}
 		return res.status(404).json({ error: 'No response found', prompt });
 	} else {
 		return res.status(403).json({ error: 'Authroization error' });
